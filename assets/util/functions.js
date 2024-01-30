163ed08560252da9f3c155ab3997b012f05d89af
barel31 <barel123133@gmail.com>
2024-01-30 20:57:13 +0200

    Refactor time formatting and remove unused email validation function

--- assets/util/functions.js
+++ assets/util/functions.js
@@ -238,26 +238,3 @@ export const isTouchDevice = () =>
   "ontouchstart" in window ||
   navigator.maxTouchPoints > 0 ||
   navigator.msMaxTouchPoints > 0;
-
-/**
- * Formats the given time into a string representation.
- * @param {number} time - The time to format.
- * @param {boolean} [inSeconds=true] - Indicates whether the time is in seconds or milliseconds. Default is true.
- * @returns {string} The formatted time string.
- */
-export const formatTime = (time, inSeconds = true) => {
-  const date = new Date(inSeconds ? time * 1000 : time);
-  const isDifferentDate = date.toDateString() !== new Date().toDateString();
-  const formatDate = `${date.getDate()}/${date.getMonth() +
-    1}/${date.getFullYear()}`;
-
-  return `${date.getHours()}:${date.getMinutes()}${
-    date.getMinutes() < 10 ? '0' : ''
-  }
-  ${isDifferentDate ? ` - ${formatDate}` : ''}`;
-};
-
-export const isValidEmail = (email) => {
-  const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return re.test(email);
-};
